9702a029e425b03a9e0c5971734a69eef31e14dc
Wagish Sharma <wagishsharma2@gmail.com>
2017-11-18 20:03:06 +0530

    enhance UI

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,29 +1,41 @@
 <?php $this->load->view('template/header'); ?>
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
+<div class="modal-dialog">
+  <div class="modal-content">
+    <div class="modal-heading">
+      <h2 class="text-center">Login</h2>
+    </div>
+    <hr />
+    <div class="modal-body">
+      <div id="infoMessage"><?php echo $message;?></div>
 
-<?php echo form_open("auth/login");?>
+      <?php echo form_open("auth/login");?>
 
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
+      <p>
+        <span class="glyphicon glyphicon-user"></span>
+        <?php echo lang('login_identity_label', 'identity');?>
+        <?php echo form_input($identity);?>
+      </p>
 
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
+      <p>
+       <span class="glyphicon glyphicon-lock"></span>
+       <?php echo lang('login_password_label', 'password');?>
+       <?php echo form_input($password);?>
+     </p>
 
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
+     <p>
+      
+      <?php echo lang('login_remember_label', 'remember');?>
+      <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+    </p>
 
 
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+    <p><?php echo form_submit('submit', lang('login_submit_btn'),"class='btn btn-danger btn-lg'");?></p>
 
-<?php echo form_close();?>
+    <?php echo form_close();?>
 
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+    <p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+  </div>
+</div>
+</div>
+
