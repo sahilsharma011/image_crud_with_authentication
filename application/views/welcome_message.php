804a674fda04c3f9923834ad4e539fd96d535370
Wagish Sharma <wagishsharma2@gmail.com>
2017-12-10 02:40:27 +0530

    Remove banner, Add image post

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,11 +1,10 @@
 <?php $this->load->view('template/header'); ?>
 
-  <h2>Image banner</h2>  
+  <!--<h2>Image banner</h2>  
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
     <ol class="carousel-indicators">
       
-      <?php $i = 0;foreach($photos as $photo){
+      < ?php $i = 0;foreach($photos as $photo){
       	if($i==0){
       		echo '<li data-target="#myCarousel" data-slide-to="0" class="active"></li>';
       	}
@@ -17,11 +16,10 @@
 
     </ol>
 
-    <!-- Wrapper for slides -->
     <div class="carousel-inner">
     
       
-      <?php $i = 0; foreach ($photos as $photo){
+      < ?php $i = 0; foreach ($photos as $photo){
       	if($i == 0){
       		echo '<div class="item active">
         <a href ='.$photo->url.'>
@@ -40,7 +38,6 @@
       
     </div>
 
-    <!-- Left and right controls -->
     <a class="left carousel-control" href="#myCarousel" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left"></span>
       <span class="sr-only">Previous</span>
@@ -49,6 +46,20 @@
       <span class="glyphicon glyphicon-chevron-right"></span>
       <span class="sr-only">Next</span>
     </a>
-  </div>
+  </div>-->
+  
+  <?php foreach($photos as $photo) {
 
+echo '<div class="panel-body">
+  <img class="card-img-top" src="assets/uploads/'.$photo->name.'" alt="Card image cap">
+  <div class="card-block">
+    <h4 class="card-title">'.$photo->name.'</h4>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+  </div>
+  </div>'
+  ;
+
+}
+?>
 <?php $this->load->view('template/footer'); ?>
